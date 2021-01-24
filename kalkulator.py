3687324524abcb2d968f8c4c2c0fb3f903dee16c
Wardhanna <77399426+Wardhanna@users.noreply.github.com>
2021-01-24 19:38:40 +0800

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,3 +1,9 @@
+import datetime
+from datetime import datetime,time
+import time
+
+
+
 def tambah(a,b):
     tambah = a + b
     return tambah
@@ -15,6 +21,29 @@ def bagi(a,b):
     return bagi
 
 while True:
+    jamLokal = time.asctime(time.localtime(time.time()))
+    jam = time.localtime().tm_hour
+
+    jamLokal = time.asctime(time.localtime(time.time()))
+    jam = time.localtime().tm_hour
+
+    print("=" * 60 + "\n" + "=" * 24 + "-|" + "Kalkulator" + "|-" + "=" * 22)
+    print("=" * 16 + "-|" + jamLokal + "|-" + "=" * 16)
+    if 0 <= jam <= 6:
+        print("=" * 21 + "-<|Selamat Subuh|>-" + "=" * 20)
+
+    elif 6 <= jam <= 12:
+        print("=" * 21 + "-<|Selamat Pagi|>-" + "=" * 21)
+
+    elif 12 <= jam <= 14:
+        print("=" * 21 + "-<|Selamat Siang|>-" + "=" * 20)
+
+    elif 14 <= jam <= 18:
+        print("=" * 22 + "-|Selamat Sore|-" + "=" * 22)
+
+    elif 18 <= jam <= 24:
+        print("=" * 21 + "-<|Selamat Malam|>-" + "=" * 20)
+    print(60 * "=")
     print('\n\t============= KALKULATOR SEDERHANA =============')
     a = int(float(input('Masukan Bilangan Pertama : ')))
     b = int(float(input('Masukan Bilangan Kedua   : ')))
